5fd0eff3143d3707056f6f9df997af323a305c40
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-09 15:27:57 -0600

    MVP reqs met along with query stretch

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,10 +3,10 @@ const db = require('../data/db-config.js');
 module.exports = {
     find,
     findById,
-    // findSteps,
-    // add,
-    // update,
-    // remove
+    findSteps,
+    add,
+    update,
+    remove
 };
 
 function find() {
@@ -17,4 +17,29 @@ function findById(id){
     return db('schemes')
         .where({ id })
         .first();
+}
+
+function findSteps(schemeId) {
+    return   db('steps')
+    .join('schemes', 'steps.scheme_id', '=', 'schemes.id')
+    .where({ scheme_id : schemeId })
+    .select('schemes.scheme_name', 'steps.*')
+    .orderBy('step_number')
+}
+
+function add(scheme) {
+    return db('schemes')
+    .insert(scheme, "id")
+}
+
+function update(changes, scheme_id){
+    return db('schemes')
+        .where( 'id', Number(scheme_id) )
+        .update(changes)
+}
+
+function remove(scheme_id) {
+    return db('schemes')
+        .where('id', scheme_id)
+        .del()
 }
